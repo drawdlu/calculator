3a85aefe1253647c8c0d72d3631dd8989a7801ee
drawdlu <152858099+drawdlu@users.noreply.github.com>
2024-09-12 22:13:17 +0800

    Create function to operate on numbers

--- script.js
+++ script.js
@@ -9,4 +9,17 @@ function addValue(event) {
     alert(event.target.textContent);
 }
 
+function operate(num1, num2, operand) {
+    switch (operand) {
+        case '+':
+            return +num1 + +num2;
+        case '-': 
+            return +num1 - +num2;
+        case '/':
+            return +num1 / +num2;
+        case '*': 
+            return +num1 * +num2;
+    }
+}
+
 listenToClicks();
