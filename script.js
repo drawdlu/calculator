f86132bab91ca768a6fc5b8750c752e6c1d3e7e8
drawdlu <152858099+drawdlu@users.noreply.github.com>
2024-09-12 22:22:54 +0800

    Create function to interpret button press

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 function listenToClicks () {
     const calcButtons = document.querySelectorAll('.button');
     calcButtons.forEach( (button) => {
-        button.addEventListener('click', addValue)
+        button.addEventListener('click', interpretClicks)
     })
 }
 
-function addValue(event) {
-    alert(event.target.textContent);
+let firstNum;
+let secondNum;
+let operand;
+
+function interpretClicks(event) {
+    const char = event.target.textContent
+
+    if (char === 'C') {
+        // call clear function
+    } else if (char === 'Del') {
+        // call backspace function
+    } else if (char.match(/\d/)) {
+        // save or add digit to current one
+    } else if (char === '.') {
+        // add a decimal to current digit or ignore if there is already one
+    } else if (char.match(/[\+\-\*\/]/)) {
+        // add operand and get ready for second number
+    } else if (char === `=`) {
+        // call operate when operands and operator is complete
+    }
 }
 
 function operate(num1, num2, operand) {
