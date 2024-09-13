d35a8f4fb7e41ed9a099419e7de4cf2a1a1c805d
drawdlu <152858099+drawdlu@users.noreply.github.com>
2024-09-13 16:02:32 +0800

    feat: Handle operations using only operands button

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
+const displayArea = document.querySelector('.display');
+
+let previousNum = '';
+let currentNum = '';
+let operand = '';
+
 function listenToClicks () {
     const calcButtons = document.querySelectorAll('.button');
     calcButtons.forEach( (button) => {
         button.addEventListener('click', interpretClicks)
     })
 }
-
-let previousNum;
-let currentNum;
-let operand;
 
 function interpretClicks(event) {
     const char = event.target.textContent
@@ -17,13 +19,12 @@ function interpretClicks(event) {
     } else if (char === 'Del') {
         // call backspace function
     } else if (char.match(/\d/)) {
-        // save or add digit to current one
-        // display digit
         displayNum(char);
+        saveDigit(char);
     } else if (char === '.') {
         // add a decimal to current digit or ignore if there is already one
     } else if (char.match(/[\+\-\*\/]/)) {
-        // add/change/operate operand and get ready for second number
+        clickedOperand(char);
     } else if (char === `=`) {
         // call operate when operands and operator is complete
     }
@@ -59,8 +60,46 @@ function divide(num1, num2) {
 }
 
 function displayNum(num) {
-    const displayArea = document.querySelector('.display');
-    displayArea.textContent += num;
+    if (!operand && !currentNum) {
+        clearDisplay();
+        displayArea.textContent += num;
+    } else if (operand && !currentNum) {
+        clearDisplay();
+        displayArea.textContent += num;
+    } else {
+        displayArea.textContent += num;
+    }
+}
+
+function saveDigit(digit) {
+    if (!previousNum) {
+        previousNum += digit;
+    } else {
+        currentNum += digit;
+    }
+}
+
+function clickedOperand(char) {
+    if (!previousNum) {
+        return;
+    } else if (!operand && !currentNum) {
+        operand = char;
+    } else if (operand) {
+        if (currentNum) {
+            previousNum = operate(previousNum, currentNum, operand);
+        } else {
+            previousNum = operate(previousNum, previousNum, operand);
+        }
+
+        currentNum = '';
+        operand = char;
+        clearDisplay();
+        displayNum(previousNum);
+    }
+}
+
+function clearDisplay() {
+    displayArea.textContent = '';
 }
 
 listenToClicks();
